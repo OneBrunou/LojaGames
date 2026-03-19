ce92328d8dd8d8c0e54f76c30b37f1ed99a7b79d
OneBrunou <ms2689992@gmail.com>
2026-03-19 17:13:54 -0300

    feat: Criado controller produto

---
+++ LojaGame/Controllers/ProdutoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LojaGame.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Produto()
+        {
+            return View();
+        }
+    }
+}
